08e31e97f977f20bc9063102accef035701bdee2
Traun Leyden <tleyden@couchbase.com>
2017-01-10 15:16:12 -0800

    Update readme, add stub

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# mobile-devops
+This repo contains Jenkins scripts for running certain build/automation tasks for Couchbase Mobile
+
---
+++ sgload_perf_test.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+
+# This is the Jenkins shell script for running an sgload based performance test
+
+print("hello world")
